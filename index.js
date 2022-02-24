7e2f67b92cbc72f99bcb88ee792ee3efa8233d5e
Mario Rodriguez <marioalejandro130@gmail.com>
2022-02-23 20:04:23 -0600

    The app do what its supouse to do, but is missing to improve and clean code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//=========================================================//
+//==============Variable declarations=====================//
+//========================================================//
 let services = []
 const washCarBtn = document.getElementById('washCar-btn')
 const mowLawnBtn = document.getElementById('mowLawn-btn')
@@ -10,74 +13,64 @@ const removeEl = document.getElementById('remove-el')
 let sum = 0;
 let rendered = false
 
-const washCar = {
-    name: "Wash Car",
-    price: 10
-}
+const washCar = {name: "Wash Car", price: 10}
+const mowLawn = {name: "Mow Lawn", price: 20}
+const pullWeeds = {name: "Pull Weeds", price: 30}
 
-const mowLawn = {
-    name: "Mow Lawn",
-    price: 20
-}
-
-const pullWeeds = {
-    name: "Pull Weeds",
-    price: 30
-}
-
+//=================================================//
+//===============Events============================//
+//=================================================//
 washCarBtn.addEventListener('click', function(){
     rendered = iterateArray(washCar)
     if (rendered === false) {
         pushToArray(washCar)
-        render()
-        total()   
+        render(washCar.name, washCar.price)
     }
-    
 })
 
 mowLawnBtn.addEventListener('click', function(){
     rendered = iterateArray(mowLawn)
     if (rendered === false) {
         pushToArray(mowLawn)
-        render()
-        total()   
+        render(mowLawn.name, mowLawn.price) 
     }
 })
 pullWeedsBtn.addEventListener('click', function(){
     rendered = iterateArray(pullWeeds)
     if (rendered === false) {
         pushToArray(pullWeeds)
-        render()
-        total()   
+        render(pullWeeds.name, pullWeeds.price) 
     }
 })
 
 sendBtn.addEventListener("click", resetList)
 
-//functions
+//=======================================================//
+//===================FUNCTIONS===========================//
+//=======================================================//
 
+//Function to render the invoice
+function render(elem, elemPrice) {
+    let li = document.createElement('li')
+    for (let i = 0; i < services.length; i++) {
+        li.innerHTML = `<p>${elem}<span class="remove" id="remove-el">remove</span></p> <p><span>$</span>${services[i].price}</p>`
+    }
+    sum += elemPrice
+    // console.log(sum)
+    totalAmount.innerHTML = `<p><span>$</span>${sum}</p>`
+    list.appendChild(li)
+}
+
+//==========================================//
 //Function to push to array
+//==========================================//
 function pushToArray(el) {
     services.push(el)
 }
 
-//function to add total
-function total() {
-    for(let i = 0; i < services.length; i++){
-        sum += services[i].price
-        totalAmount.innerHTML = `<p><span>$</span>${sum}</p>`
-    }
-    sum = 0;
-}
-
-function render() {
-    let li = document.createElement('li')
-    for (let i = 0; i < services.length; i++) {
-        li.innerHTML = `<p>${services[i].name}<span class="remove" id="remove-el">remove</span></p> <p><span>$</span>${services[i].price}</p>`
-    }
-    list.appendChild(li)
-}
-
+//========================================================//
+//Function to know if the element is already in the array
+//========================================================//
 function iterateArray(n) {
     for (let i = 0; i < services.length; i++) {
         if (services[i].name == n.name ) {
@@ -86,12 +79,36 @@ function iterateArray(n) {
     } return false
 }
 
+//========================================================//
 //function reset list
-
+//========================================================//
 function resetList() {
     list.innerHTML = ""
-    totalAmount.innerHTML = ""
+    totalAmount.innerHTML = "<p>$0</p>"
     services = []
+    sum = 0
 }
 
+//=======================================================//
+//Function to remove elements
+//=======================================================//
+function remove(e) {
+    let el = e.target
+    var itemSelected = el.parentNode
+    var listed = itemSelected.parentNode
+    list.removeChild(listed)
 
+    if(listed.textContent === "Wash Carremove $10"){
+        services = services.filter(service => service.price != 10)
+        sum = services.reduce((acc, num) => acc + num.price, 0)
+    } else if(listed.textContent === "Mow Lawnremove $20"){
+        services = services.filter(service => service.price != 20)
+        sum = services.reduce((acc, num) => acc + num.price, 0)
+    } else if(listed.textContent === "Pull Weedsremove $30"){
+        services = services.filter(service => service.price != 30)
+        sum = services.reduce((acc, num) => acc + num.price, 0)
+    }
+    totalAmount.innerHTML = `<p><span>$</span>${sum}</p>`
+} 
+
+list.addEventListener('click', remove)
